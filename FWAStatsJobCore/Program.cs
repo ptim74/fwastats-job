1aa69990dc4424f0cfbbc7dc001fb7fbfe1beff0
Petri Timperi <ptim74@hotmail.com>
2017-10-15 23:46:15 +0300

    Add project files.

---
+++ FWAStatsJobCore/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FWAStatsJobCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
